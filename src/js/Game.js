c45c5a4cc9b3416979671cb63aa0e9864708abbd
Oleh Karnaukhov <karnauhov.oleg@gmail.com>
2022-07-28 19:06:51 +0300

    Was finished game logic and draft UI

--- src/js/Game.js
+++ src/js/Game.js
@@ -2,65 +2,128 @@
 // Main module of business logic
 var Game = (function () {
 
+  // UI module
   var _ui = null;
-  var _gameState = {
+
+  // Game state
+  var _gs = {
     exitUrl: "https://play.works/",
     status: 0, //-1: EXIT, 0: NOT_STARTED, 1: WAIT, 2: SWITCH_TURN, 3: HUMAN_TURN, 4: CPU_TURN, 5: HUMAN_WIN, 6: CPU_WIN, 7: DEADLOCK
     whoseTurn: 0, //0: CPU, 1: Human
-    cpuSign: "X",
-    humanSign: "O",
-    field: [null, null, null, null, null, null, null, null, null], //Row1: 0, 1, 2; Row2: 3, 4, 5; Row3: 6, 7, 8;
+    cpuSign: "x",
+    humanSign: "o",
+    fld: [null, null, null, null, null, null, null, null, null], //Row1: 0, 1, 2; Row2: 3, 4, 5; Row3: 6, 7, 8;
   };
+
+  var _generateRandomPositive = function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  var _getRandomEmptyFieldIndex = function() {
+    var emptyIndexes = [];
+    for (var i = 0; i < _gs.fld.length; i++) {
+      if (_gs.fld[i] == null) {
+        emptyIndexes.push(i);
+      }
+    }
+    return emptyIndexes[_generateRandomPositive(1, emptyIndexes.length) - 1];
+  };
+
+  var _findDoubleValue = function(val, arrIndexes) {
+    var arrValues = [];
+    for (var j = 0; j < arrIndexes.length; j++) {
+      arrValues.push(_gs.fld[arrIndexes[j]]);
+    }
+    var counter = 0;
+    var emptyIndex = -1;
+    for (var i = 0; i < arrValues.length; i++) {
+      if (arrValues[i] == null) {
+        emptyIndex = arrIndexes[i];
+      } else if (arrValues[i] == val) {
+        counter++;
+      }
+    }
+    return counter >= 2 ? emptyIndex : -1;
+  }
+
+  var _findPossibleWinIndex = function(val) {
+    var result = -1;
+    var checkLinesArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (var i = 0; i < checkLinesArray.length; i++) {
+      result = _findDoubleValue(val, checkLinesArray[i]);
+      if (result != -1) {
+        return result;
+      }
+    }
+    return result;
+  }
+
+  var _calcCpuStep = function() {
+    // Check own possible wins
+    var ownResult = _findPossibleWinIndex(0);
+    if (ownResult != -1) {
+      return ownResult;
+    }
+
+    // Check human possible wins
+    var humanResult = _findPossibleWinIndex(1);
+    if (humanResult != -1) {
+      return humanResult;
+    }
+
+    // Set random place
+    return _getRandomEmptyFieldIndex();
+  }
 
   var _setRandomTurn = function() {
     if (Math.random() < 0.5) {
-      _gameState.whoseTurn = 0;
-      _gameState.cpuSign = "X";
-      _gameState.humanSign = "O";
+      _gs.whoseTurn = 0;
+      _gs.cpuSign = "x";
+      _gs.humanSign = "o";
     } else {
-      _gameState.whoseTurn = 1;
-      _gameState.cpuSign = "O";
-      _gameState.humanSign = "X";
+      _gs.whoseTurn = 1;
+      _gs.cpuSign = "o";
+      _gs.humanSign = "x";
     }
   };
 
   var _updateGameStateMachine  = function() {
-    switch (_gameState.status) {
+    switch (_gs.status) {
       case -1: // EXIT
         stop();
-        _ui.exit(_gameState.exitUrl);
+        _ui.exit(_gs.exitUrl);
         return;
       case 0: // NOT_STARTED
         _setRandomTurn();
-        _gameState.status = 1; // WAIT
+        _gs.status = 1; // WAIT
         _ui.showStartQuestion(startAnswer);
         break;
       case 2: // SWITCH_TURN
-        if (_gameState.whoseTurn) {
-          _gameState.status = 3; // HUMAN_TURN
+        if (_gs.whoseTurn) {
+          _gs.status = 3; // HUMAN_TURN
         } else {
-          _gameState.status = 4; // CPU_TURN
+          _gs.status = 4; // CPU_TURN
         }
         break;
       case 3: // HUMAN_TURN
-        _gameState.status = 1; // WAIT
-        _ui.beginHumanTurn(humanAnswer);
+        _gs.status = 1; // WAIT
+        _ui.beginHumanTurn(humanAnswer, _gs.fld[0], _gs.fld[1], _gs.fld[2], _gs.fld[3], _gs.fld[4], _gs.fld[5], _gs.fld[6], _gs.fld[7], _gs.fld[8], _gs.cpuSign, _gs.humanSign);
         break;
       case 4: // CPU_TURN
-        _gameState.status = 1; // WAIT
+        _gs.status = 1; // WAIT
         beginCpuTurn(cpuAnswer);
         break;
       case 5: // HUMAN_WIN
-        _gameState.status = 1; // WAIT
-        _ui.humanWin(endGame);
+        _gs.status = 1; // WAIT
+        _ui.humanWin(endGame, _gs.fld[0], _gs.fld[1], _gs.fld[2], _gs.fld[3], _gs.fld[4], _gs.fld[5], _gs.fld[6], _gs.fld[7], _gs.fld[8], _gs.cpuSign, _gs.humanSign);
         break;
       case 6: // CPU_WIN
-        _gameState.status = 1; // WAIT
-        _ui.cpuWin(endGame);
+        _gs.status = 1; // WAIT
+        _ui.cpuWin(endGame, _gs.fld[0], _gs.fld[1], _gs.fld[2], _gs.fld[3], _gs.fld[4], _gs.fld[5], _gs.fld[6], _gs.fld[7], _gs.fld[8], _gs.cpuSign, _gs.humanSign);
         break;
       case 7: // DEADLOCK
-        _gameState.status = 1; // WAIT
-        _ui.deadlock(endGame);
+        _gs.status = 1; // WAIT
+        _ui.deadlock(endGame, _gs.fld[0], _gs.fld[1], _gs.fld[2], _gs.fld[3], _gs.fld[4], _gs.fld[5], _gs.fld[6], _gs.fld[7], _gs.fld[8], _gs.cpuSign, _gs.humanSign);
         break;
       default:
         break;
@@ -69,7 +132,7 @@ var Game = (function () {
 
   var loop = function() {
     _updateGameStateMachine();
-    _ui.updateFrame(_gameState.status);
+    _ui.updateFrame(_gs.status, _gs.fld[0], _gs.fld[1], _gs.fld[2], _gs.fld[3], _gs.fld[4], _gs.fld[5], _gs.fld[6], _gs.fld[7], _gs.fld[8], _gs.cpuSign, _gs.humanSign);
   };
 
   var start = function (ui) {
@@ -82,55 +145,76 @@ var Game = (function () {
 
   var startAnswer = function(isPlay) {
     if (isPlay) {
-      _gameState.status = 2; // SWITCH_TURN
+      _gs.status = 2; // SWITCH_TURN
     } else {
-      _gameState.status = -1; // EXIT
+      _gs.status = -1; // EXIT
     }
   };
 
   var humanAnswer = function(index) {
-    _gameState.field[index] = 1
+    _gs.fld[index] = 1;
     checkEndGame();
   };
 
   var cpuAnswer = function(index) {
-    _gameState.field[index] = 0
+    _gs.fld[index] = 0;
     checkEndGame();
   };
 
   var beginCpuTurn = function(callbackResult) {
     if(callbackResult) {
-      // TODO CPU turn
-      console.log("TODO: CPU turn");
-      setTimeout(callbackResult, 1000, 7); // 7 for test example
+      var resultIndex = _calcCpuStep();
+      setTimeout(callbackResult, 300, resultIndex);
+    } else {
+      throw Error('Function needs callback');
     }
   };
 
   var checkEndGame = function() {
-    var result = checkField()
+    var result = checkField();
     if (result == 0) {
-      _gameState.status = 6; // CPU_WIN
+      _gs.status = 6; // CPU_WIN
     } else if (result == 1) {
-      _gameState.status = 5; // HUMAN_WIN
+      _gs.status = 5; // HUMAN_WIN
     } else if (result == -1) {
-      _gameState.status = 7; //DEADLOCK
+      _gs.status = 7; //DEADLOCK
     } else { // TURN NEXT
-      _gameState.whoseTurn = _gameState.whoseTurn ? 0 : 1;
-      _gameState.status = 2; // SWITCH_TURN
+      _gs.whoseTurn = _gs.whoseTurn ? 0 : 1;
+      _gs.status = 2; // SWITCH_TURN
     }
   };
 
-  var checkField = function() { // Return null for continue, -1 for deadlock; 0 for human win, 1 for cpu win
-    // TODO check field
-    return null
+  var checkField = function() { // Return null for continue, -1 for deadlock; 0 for cpu win, 1 for human win
+    // Check winner
+    for(var win = 0; win <= 1; win++) {
+      if ((_gs.fld[0] == win && _gs.fld[1] == win && _gs.fld[2] == win) ||      //row 1
+          (_gs.fld[3] == win && _gs.fld[4] == win && _gs.fld[5] == win) ||      //row 2
+          (_gs.fld[6] == win && _gs.fld[7] == win && _gs.fld[8] == win) ||      //row 3
+          (_gs.fld[0] == win && _gs.fld[3] == win && _gs.fld[6] == win) ||      //column 1
+          (_gs.fld[1] == win && _gs.fld[4] == win && _gs.fld[7] == win) ||      //column 2
+          (_gs.fld[2] == win && _gs.fld[5] == win && _gs.fld[8] == win) ||      //column 3
+          (_gs.fld[0] == win && _gs.fld[4] == win && _gs.fld[8] == win) ||      //diag 1
+          (_gs.fld[2] == win && _gs.fld[4] == win && _gs.fld[6] == win)         //diag 2
+      ){
+        return win;
+      }
+    }
+
+    // Check deadlock
+    for (var i = 0; i < _gs.fld.length; i++) {
+      if (_gs.fld[i] == null) {
+        return null;
+      }
+    }
+    return -1;
   };
 
   var endGame = function() {
-    for (var i = 0; i < _gameState.field.length; i++) {
-      _gameState.field[i] = null;
+    for (var i = 0; i < _gs.fld.length; i++) {
+      _gs.fld[i] = null;
     }
     // Restart
-    _gameState.status = 0; // NOT_STARTED
+    _gs.status = 0; // NOT_STARTED
   };
 
   var stop = function () {
--- src/js/UIdraft.js
+++ src/js/UIdraft.js
@@ -6,9 +6,28 @@ var UIdraft = (function () {
     FPS: 30,
   };
 
-  var updateFrame = function(gameStatus) {
-    // TODO update field
-    //console.log(gameStatus);
+  var _fixVal = function(value, sign0, sign1) {
+    if (value == null) {
+      return "  ";
+    } else if (value == 0) {
+      return sign0;
+    } else {
+      return sign1;
+    }
+  };
+
+  var _currentFieldMessage  = function(f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
+    var message = "";
+    message += _fixVal(f0, s0, s1) + " | " + _fixVal(f1, s0, s1) + " | " + _fixVal(f2, s0, s1) + "\n";
+    message += "---------\n";
+    message += _fixVal(f3, s0, s1) + " | " + _fixVal(f4, s0, s1) + " | " + _fixVal(f5, s0, s1) + "\n";
+    message += "---------\n";
+    message += _fixVal(f6, s0, s1) + " | " + _fixVal(f7, s0, s1) + " | " + _fixVal(f8, s0, s1) + "\n";
+    return message;
+  };
+
+  var updateFrame = function(gameStatus, f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
+    // Nothing to update in this UI version
   };
 
   var showStartQuestion = function(callbackAnswer) {
@@ -19,41 +38,55 @@ var UIdraft = (function () {
     }
   };
 
-  var beginHumanTurn = function(callbackResult) {
+  var beginHumanTurn = function(callbackResult, f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
     if(callbackResult) {
-      // TODO human turn
-      console.log("TODO: Human turn");
-      setTimeout(callbackResult, 3000, 5); // 5 for test example
+      if (f0 == null || f1 == null || f2 == null ||
+          f3 == null || f4 == null || f5 == null ||
+          f6 == null || f7 == null || f8 == null) {
+        var result = 0;
+        do {
+          var message = "Please choose empty place index for your sign [0-8] (your sign is \"" + s1 + "\")\n";
+          message += _fixVal(f0, s0, s1) + " | " + _fixVal(f1, s0, s1) + " | " + _fixVal(f2, s0, s1) + "            0 | 1 | 2 " + "\n";
+          message += "---------          ---------\n";
+          message += _fixVal(f3, s0, s1) + " | " + _fixVal(f4, s0, s1) + " | " + _fixVal(f5, s0, s1) + "            3 | 4 | 5 " + "\n";
+          message += "---------          ---------\n";
+          message += _fixVal(f6, s0, s1) + " | " + _fixVal(f7, s0, s1) + " | " + _fixVal(f8, s0, s1) + "            6 | 7 | 8 " + "\n";
+          result = parseInt(prompt(message, 0));
+        } while (isNaN(result) || result < 0 || result > 8 || arguments[result + 1] != null);
+        callbackResult(result);
+      } else {
+        throw Error('No empty places');
+      }
     } else {
       throw Error('Function needs callback');
     }
   };
 
-  var humanWin = function(callbackEnd) {
+  var humanWin = function(callbackEnd, f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
     if(callbackEnd) {
-      // TODO human win
-      console.log("TODO: Human win");
-      setTimeout(callbackEnd, 3000);
+      var message = "Human " + "(\"" + s1 + "\")" + " win\n" + _currentFieldMessage(f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1);
+      alert(message);
+      callbackEnd();
     } else {
       throw Error('Function needs callback');
     }
   };
 
-  var cpuWin = function(callbackEnd) {
+  var cpuWin = function(callbackEnd, f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
     if(callbackEnd) {
-      // TODO CPU win
-      console.log("TODO: CPU win");
-      setTimeout(callbackEnd, 3000);
+      var message = "CPU " + "(\"" + s0 + "\")" + " win\n" + _currentFieldMessage(f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1);
+      alert(message);
+      callbackEnd();
     } else {
       throw Error('Function needs callback');
     }
   };
 
-  var deadlock = function(callbackEnd) {
+  var deadlock = function(callbackEnd, f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1) {
     if(callbackEnd) {
-      // TODO Deadlock
-      console.log("TODO: Deadlock");
-      setTimeout(callbackEnd, 3000);
+      var message = "Deadlock\n" + _currentFieldMessage(f0, f1, f2, f3, f4, f5, f6, f7, f8, s0, s1);
+      alert(message);
+      callbackEnd();
     } else {
       throw Error('Function needs callback');
     }
